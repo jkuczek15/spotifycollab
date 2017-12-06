228a80a1a0a1b56afcb425bcbb183de0ded174fc
Joe Kuczek <jkuczek@iastate.edu>
2017-12-06 00:24:02 -0600

    added function to host button

--- home/static/home/js/main.js
+++ home/static/home/js/main.js
@@ -3,4 +3,9 @@ var app = angular.module('app', []);
 app.controller('MainCtrl', ['$scope', function MainController($scope) {
     $scope.title = 'Some text here';
     $scope.selectedTab = 'Home';
+
+    $scope.switchTabs = function(newTab) {
+        $scope.selectedTab = newTab;
+    }// end function switchTabs
+
 }]);
